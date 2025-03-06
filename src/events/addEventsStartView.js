9051cabe91cbd9935e6be8754136cbe6167514d7
iversson-estudos <i versson.estudos@gmail.com>
2025-03-06 15:47:15 -0300

    working on the routing, forgot to use TDD for these steps, will get back to it

---
+++ src/events/addEventsStartView.js
@@ -0,0 +1,5 @@
+function addEventsStartView() {
+  const playerName = document.getElementById("playerName").textContent;
+  const startGameBtn = document.getElementById("start");
+  startGameBtn.addEventListener("click", () => {});
+}
---
+++ src/events/startGame.js
@@ -0,0 +1,6 @@
+const Player = require("../classes/player.js");
+
+function startGame(playerName) {
+  const humanPlayer = new Player(playerName);
+  const aiPlayer = new Player();
+}

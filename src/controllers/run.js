e087fb2a1d983161abb46206fde4ae30428761be
iversson-estudos <i versson.estudos@gmail.com>
2025-03-10 17:22:06 -0300

    working on a random ship placement

--- src/controllers/run.js
+++ src/controllers/run.js
@@ -11,6 +11,7 @@ function run() {
 
   //starts game
   game.startGame();
+  placeRandomShips();
 }
 
 function attack(cell) {
@@ -20,4 +21,17 @@ function attack(cell) {
   game.humanAttack(x, y);
 }
 
+function placeRandomShips(player) {
+  //log of ships already placed, so we dont have colision
+  let placedShips = [];
+  //how many ships will be placed
+  const numberOfShips = 10;
+
+  for (let i = 0; i < numberOfShips; i++) {
+    const x = Math.floor(Math.random() * 8);
+    const y = Math.floor(Math.random() * 8);
+    console.log(x, y);
+    //check colisions
+  }
+}
 export { run, attack };

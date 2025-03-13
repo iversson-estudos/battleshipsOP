799a24ad17597b4a79b25610350f255b8247ea82
iversson-estudos <i versson.estudos@gmail.com>
2025-03-13 19:58:05 -0300

    2 functions for AI Attacks, working

--- src/classes/game.js
+++ src/classes/game.js
@@ -17,14 +17,40 @@ class Game {
     if (this.turn === "Player") {
       this.aiPlayer.gameboard.receiveAttack(x, y);
       this.startGame();
-      this.changeTurn(); //this needs to change to "AI"/Player"
+      this.changeTurn();
+      this.aiGenerateCoordinates(); //this needs to change to "AI"/Player"
     }
   }
-  aiAttack() {
+  aiAttack(x, y) {
     if (this.turn === "AI") {
       this.humanPlayer.gameboard.receiveAttack(x, y);
+      this.startGame();
+      this.changeTurn();
     }
   }
+  aiGenerateCoordinates() {
+    this.attacksLog = [];
+    this.attackSucceed = 0;
+
+    while (this.attackSucceed === 0) {
+      const x = Math.floor(Math.random() * 8);
+      const y = Math.floor(Math.random() * 8);
+
+      //check colisions
+      let found = 0;
+      for (let f = 0; f < this.attacksLog.length; f++) {
+        if (this.attacksLog[f][0] === x && this.attacksLog[f][1] === y) {
+          found = 1;
+        }
+      }
+      if (!found) {
+        this.attacksLog.push([x, y]);
+        this.attackSucceed = 1;
+        this.aiAttack(x, y);
+      }
+    }
+  }
+
   changeTurn() {
     if (this.turn === "Player") {
       this.turn = "AI";
